91ab2c9be97d3f2f3a4c635850c0f41c4832c9e9
MahdiTavakol <mahditavakol90@gmail.com>
2025-06-29 10:14:43 +0100

    Some modifications of the chapter-8

--- Codes/Ch-8/Chapter-8/Chapter-8/EigenStlExamples.cpp
+++ Codes/Ch-8/Chapter-8/Chapter-8/EigenStlExamples.cpp
@@ -126,7 +126,7 @@ void log_rtns_rowwise_colwise()
 	cout << prices_to_returns << "\n" << "\n";
 
 	for (auto row : prices_to_returns.rowwise())
-		std::ranges::transform(row, row.begin(), [](double x) {return std::log(x););
+		std::ranges::transform(row, row.begin(), [](double x) {return std::log(x); });
 
 	cout << "Logs of prices: " << "\n";
 	cout << std::fixed << std::setprecision(6) << std::right << prices_to_returns << "\n\n";
@@ -161,6 +161,99 @@ void log_rtns_rowwise_colwise()
 		<< monthly_returns(j) * 100 << "%\n";
 
 	cout << std::endl << std::endl;
+}
+
+class SquareRoot
+{
+public:
+	double operator()(double x) const
+	{
+		return std::sqrt(x);
+	}
+};
+
+void eigen_mtx_unary_expr()
+{
+	cout << format("\n*** eigen_mtx_unary_expr() ***\n\n");
+
+	MatrixXd vals_01
+	{
+		{9.0, 8.0, 7.0},
+		{3.0, 2.0, 1.0},
+		{9.5, 8.5, 7.5},
+		{3.5, 2.5, 1.5}
+	};
+
+	cout << "Origina mtx: " << std::endl;
+	cout << vals_01 << std::endl << std::endl;
+
+	cout << "Square each element with unaryExpr(.): " << std::endl;
+	vals_01 = vals_01.unaryExpr([](double x) {return x * x; });
+	cout << "Tye of vals_01 (mtx): " << typeid(vals_01).name() << "vals now = \n" << vals_01 << "\n\n";
+
+	cout << "Now take square root of each element in the updated vals mtx with unaryExpr(.): " << "\n";
+	SquareRoot sq{};
+	vals_01 = vals_01.unaryExpr(sq);
+	cout << "vals type: " << typeid(vals_01).name() << "\n" << vals_01 << "\n\n";
+
+	cout << "Results stored as new MatrixXd objects: " << std::endl;
+	MatrixXd vals_02{ vals_01.unaryExpr([](double x) {return x * x; }) };
+	cout << vals_02 << std::endl << std::endl;
+
+	MatrixXd vals_03 = vals_02.unaryExpr(sq);
+	cout << vals_03 << std::endl << std::endl;
+}
+
+void eigen_math_array_fcns()
+{
+	cout << std::endl << std::format("\n*** eigen_math_array_fcns() ***") << "\n";
+
+	MatrixXd vals_01
+	{
+		{9.0, 8.0, 7.0},
+		{3.0, 2.0, 1.0},
+		{9.5, 8.5, 7.5},
+		{3.5, 2.5, 1.5}
+	};
+
+	cout << "Square each element with .array.square(.): " << "\n";
+	vals_01 = vals_01.array().square();
+	cout << vals_01 << "\n\n";
+
+	cout << "Now take square root of each element with .array().sqrt(): " << "\n";
+	vals_01 = vals_01.array().sqrt();
+	cout << vals_01 << "\n\n";
+
+	cout << "Results stored as new MatrixXd objects: " << "\n";
+	MatrixXd vals_squared{ vals_01.array().square() };
+	cout << vals_squared << "\n\n";
+
+	cout << "Was the vals_01 matrix moified in the process? (ans :no)\n" << vals_01 << "\n\n";
+
+	MatrixXd vals_squared_sqrt = vals_squared.array().sqrt();
+	cout << vals_squared_sqrt << "\n\n";
+
+
+	MatrixXd prices_to_returns
+	{
+		{25.5, 8.0, 70.5},
+		{31.0, 7.5, 71.0},
+		{29.5, 8.5, 77.5},
+		{33.5, 5.5, 71.5},
+		{26.5, 9.5, 72.5},
+		{34.5, 8.5, 75.5},
+		{28.5, 9.0, 72.0},
+		{23.5, 7.5, 73.5},
+		{28.0, 8.0, 72.5},
+		{31.5, 9.0, 73.0},
+		{32.5, 9.5, 74.5}
+	};
+
+	prices_to_returns = prices_to_returns.array().log();
+	cout << "Logs of prices using .array().log()\n";
+	cout << "(However, would need to apply std::adjacent_difference etc):\n";
+	cout << prices_to_returns << "\n\n";
+
 }
 
 
